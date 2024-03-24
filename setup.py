0881cb427605e761b513f32e12d393889a297e38
japonkei <yahyajapan@gmail.com>
2024-03-24 22:59:21 +0100

    -

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-exec(open('denoising_diffusion_pytorch/version.py').read())
+exec(open('code/version.py').read())
 
 setup(
   name = 'denoising-diffusion-pytorch',
